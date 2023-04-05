ed75bfd3eea758d0b7160ecce6601be390af3eca
Pepsi_Man <mr.doomsday07@gmail.com>
2023-04-05 19:16:09 +0000

    sjxdwc

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ const about = document.querySelector('#about')
 const contact = document.querySelector('#contact')
 const aboutContent = document.querySelector('#about-content')
 const contactContent = document.querySelector('#contact-content')
+const portfolio = document.querySelector('#portfolio')
+const portfolioContent = document.querySelector('#portfolio-content')
 
 about.addEventListener('click', () => {
   const aboutBox = new WinBox({
@@ -15,7 +17,7 @@ about.addEventListener('click', () => {
     left: 50,
     mount: aboutContent,
     onfocus: function () {
-      this.setBackground('#00ccff')
+      this.setBackground('#003cff')
     },
     onblur: function () {
       this.setBackground('#777')
@@ -28,13 +30,32 @@ contact.addEventListener('click', () => {
     title: 'Contact Me',
     width: '400px',
     height: '400px',
-    top: 150,
+    top: 50,
     right: 50,
     bottom: 50,
-    left: 250,
+    left: 50,
     mount: contactContent,
     onfocus: function () {
-      this.setBackground('#00ccff')
+      this.setBackground('#003cff')
+    },
+    onblur: function () {
+      this.setBackground('#777')
+    },
+  })
+})
+
+portfolio.addEventListener('click', () => {
+  const aboutBox = new WinBox({
+    title: 'Portfolio',
+    width: '400px',
+    height: '400px',
+    top: 50,
+    right: 50,
+    bottom: 50,
+    left: 50,
+    mount: portfolioContent,
+    onfocus: function () {
+      this.setBackground('#003cff')
     },
     onblur: function () {
       this.setBackground('#777')
